df8899bf6d22313712afe7022d956d4a8c59afdb
STRE4MY <niklas.belieres@gmail.com>
2026-02-05 17:57:44 +0100

    Add Jest test setup and cleaned up

---
+++ test/files/discover.test.ts
@@ -0,0 +1,20 @@
+import { discoverProject } from "../../packages/core/project/discoverProject";
+import * as path from "node:path";
+import * as fs from "node:fs";
+import * as os from "node:os";
+
+    test("finds nearest tsconfig.json", () => {
+        const fixtureRoot = path.resolve("test/fixtures/basic");
+        const testDiscoverProject = discoverProject(path.join(fixtureRoot, "src"));
+        expect(testDiscoverProject.tsconfigPath).toBe(path.join(fixtureRoot, "tsconfig.json"));
+        expect(testDiscoverProject.rootDir).toBe(fixtureRoot);
+    })
+
+    test("throws error when no tsconfig.json is found", () => {
+    const tmp = fs.mkdtempSync(path.join(os.tmpdir(), "dcr-no-tsconfig-"));
+        try {
+             expect(() => discoverProject(tmp)).toThrow("tsconfig.json not found");
+        } finally {
+             fs.rmSync(tmp, { recursive: true, force: true });
+    }
+    })
